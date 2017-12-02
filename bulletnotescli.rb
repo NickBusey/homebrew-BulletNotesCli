cbb74aa901b0d6e94a938b39c5f74d566b687456
Nick Busey <nickabusey@gmail.com>
2017-12-01 19:38:02 -0700

    Path fix

--- bulletnotescli.rb
+++ bulletnotescli.rb
@@ -1,7 +1,7 @@
 class Bulletnotescli < Formula
   desc "A command line tool to manage your notes"
   homepage "https://github.com/NickBusey/BulletNotes"
-  url "https://github.com/NickBusey/BulletNotes/raw/master/archive/bulletnotescli-1.0.0.tar.gz"
+  url "https://github.com/NickBusey/homebrew-BulletNotesCli/raw/master/archive/bulletnotescli-1.0.0.tar.gz"
   sha256 "185b95e5a441708df51a195b92526048ce1c3ae2a4be5ff6cc9c239dea2655b1"
   version "1.0.0"
 
